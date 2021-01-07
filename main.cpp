f0d458ec4fa489bf035530f9afb963c1ba2977b4
Jarosław Bieniek <justincompspec@gmail.com>
2021-01-07 21:21:00 +0100

    Update main

--- main.cpp
+++ main.cpp
@@ -12,24 +12,22 @@
 #include <utils/command_line.h>
 
 int main(int argc, char *argv[]) {
-    // dictionary::Dictionary d{};
-    // d.insert("g", "8");
-    // d.insert("e", "4");
-    // d.insert("k", "5");
-    // d.insert("v", "2");
-    // d.insert("a", "7");
-    // d.insert("y", "0");
-    // d.insert("i", "9");
-    // d.insert("p", "1");
-    // d.insert("j", "3");
-    // d.insert("t", "6");
-    // parallel::Finder f{d};
-    // const auto r = f.parallel_find("j", 5);
-    // std::cout << r.first << std::endl;
-    // return 0;
-
-#define CHUNKSIZE   10
-#define N       100
+    dictionary::Dictionary d{};
+    d.insert("g", "8");
+    d.insert("e", "4");
+    d.insert("k", "5");
+    d.insert("v", "2");
+    d.insert("a", "7");
+    d.insert("y", "0");
+    d.insert("i", "9");
+    d.insert("p", "1");
+    d.insert("j", "3");
+    d.insert("t", "6");
+    d.insert("q", "-");
+    parallel::Finder f{};
+    const auto r = f.parallel_find(d, "p", 2);
+    std::cout << r.first << std::endl;
+    return 0;
 
 //int nthreads, tid, i, chunk;
 //float a[N], b[N], c[N];
